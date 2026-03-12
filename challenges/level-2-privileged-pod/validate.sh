0d630e4cf7aad3a471673e8e272aaefaecade98e
lordofduckkk <egorwinner2007@yandex.ru>
2026-03-13 01:13:39 +0300

    Update validate.sh

--- challenges/level-2-privileged-pod/validate.sh
+++ challenges/level-2-privileged-pod/validate.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 EXPECTED="HTK{pr1v1l3g3d_c0nta1n3r_3sc4p3}"
 if [ "$1" == "$EXPECTED" ]; then
-    echo "✅ Верно! Корпус Б гордиться тобой!"
+    echo "Верно! Корпус Б гордиться тобой!"
     exit 0
 else
-    echo "❌ Неверно"
+    echo "Неверно"
     exit 1
 fi
